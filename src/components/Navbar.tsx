c7db2fb05b9bad9ac52fe8d22d9fcc6ed56a55fe
David Shaw <dshaw11@utk.edu>
2024-10-14 14:10:17 -0400

    cleared out default stuff, added favicon, imported header from next test app

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import power_t from "../assets/images/power_t_logo.png"
+
+export default function NavBar() {
+    return (
+        <nav className="h-115 w-full flex flex-row items-center">
+            <img 
+                src={power_t}
+                width={72}
+                height={72}
+                alt="power T"
+                className="flex-none p-1"
+            />
+            <a className="grow text-3xl text-utk-smokey" href="https://lib.utk.edu">Libraries Database Search</a>
+            <a className="flex-none text-3xl text-utk-smokey px-3" href="/">Home</a>
+        </nav>
+    )
+}

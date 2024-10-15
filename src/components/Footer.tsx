e1fe0522279ca628f372d665df6bb7bee8a10de9
David Shaw <dshaw11@utk.edu>
2024-10-15 16:06:23 -0400

    started styling the input group

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import CampusLogo from "./CampusLogo";
+
+// Need to add Web Speech API in the future
 
 const Footer = () => {
     return (
@@ -15,12 +18,26 @@ const Footer = () => {
                     </p>
                 </div>
             </div>
-            <div className="grid grid-rows-2">
-                <div>
-                    Input group
-                </div>
-                <div className="grid grid-cols-4">
-                    
+            <div className="grid grid-rows-2 py-2 lg:py-0">
+                <form id="utk_seek" method="GET" action="https://search.utk.edu/search" className='flex justify-center pt-3 px-2'>
+                    <div className="">
+                        <input type="text" id="utk_seek" name="q" onFocus={(e) => {
+                            if (e.target.value === 'Search utk.edu') {
+                                e.target.value = '';
+                            }
+                        }} placeholder="Search utk.edu" className="form-control" title="search"  />
+                        <input name="go" type="submit" title="Submit" className="bg-utk-gray--accent" value="Search" />
+                    </div>
+                </form>
+                <div className="grid grid-cols-4 gap-1 text-utk-smokey text-xs py-1 px-2">
+                    <Link className='' to="http://calendar.utk.edu/">Events</Link>
+                    <Link className='' to="http://www.utk.edu/alpha/">A-Z </Link>
+                    <Link className='' to="http://www.utk.edu/admissions/">Apply</Link>
+                    <Link className='' to="https://www.utk.edu/aboutut/privacy/">Privacy</Link>
+                    <Link className='' to="http://maps.utk.edu/">Map</Link>
+                    <Link className='' to="http://directory.utk.edu">Directory</Link>
+                    <Link className='' to="http://giveto.utk.edu">Give to UT</Link>
+                    <Link className='' to="https://oed.utk.edu/ada/campus-accessibility/">Accessibility</Link>
                 </div>
             </div>
         </footer>

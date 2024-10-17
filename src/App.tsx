3fa83fa2b4c927d00bf7fe799dd388bede0c9de6
David Shaw <dshaw11@utk.edu>
2024-10-17 15:31:24 -0400

    some styling of the central buttons

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,31 @@ function App() {
 
   type dbItem = {
     dbName: string,
-    page: string,
+    endpoint: string,
     dbSummary: string,
 
   }
 
-  const dbInfo:Array<dbItem> = [
+  const dbInfo: Array<dbItem> = [
     {
       dbName: "Song Database",
-      page: "/song",
-      dbSummary: "Containing the Song Index and Song Analysis Index from the George F. Devine Music Library. Includes the words and music for over 50,000 songs in 15,000 collections of songs in the Music Library. Also includes analyses, descriptions, and program notes for musical compositions in the Music Library book collection. Use in conjunction with Harold Diamond’s book, Music Analyses: An Annotated Guide to the Literature."
+      endpoint: "/song",
+      dbSummary: "Containing the Song Index and Song Analysis Index from the George F. Devine Music Library. Includes the words and music for over 50,000 songs in 15,000 collections of songs in the Music Library."
+    },
+    {
+      dbName: "Song Database",
+      endpoint: "/song",
+      dbSummary: "Containing the Song Index and Song Analysis Index from the George F. Devine Music Library. Includes the words and music for over 50,000 songs in 15,000 collections of songs in the Music Library."
+    },
+    {
+      dbName: "Song Database",
+      endpoint: "/song",
+      dbSummary: "Containing the Song Index and Song Analysis Index from the George F. Devine Music Library. Includes the words and music for over 50,000 songs in 15,000 collections of songs in the Music Library."
+    },
+    {
+      dbName: "Song Database",
+      endpoint: "/song",
+      dbSummary: "Containing the Song Index and Song Analysis Index from the George F. Devine Music Library. Includes the words and music for over 50,000 songs in 15,000 collections of songs in the Music Library."
     },
   ]
 
@@ -29,11 +44,17 @@ function App() {
         <div className='h-32 bg-utk-blue--accent'>
           <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white h-36'>Databases from UTK Libraries</h1>
         </div>
-        <div className='grid grid-col-1 sm:grid-cols-2 gap-10 justify-items-stretch py-3 mx-2'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div className='container mx-auto py-2'>
+          <div className='grid grid-col-1 sm:grid-cols-2 gap-5 justify-items-stretch py-3 mx-2'>
+            {dbInfo.map((item, index) =>
+              <Card
+                key={index}
+                dbName={item.dbName}
+                endpoint={item.endpoint}
+                summary={item.dbSummary}
+              />
+            )}
+          </div>
         </div>
       </main>
       <Footer />

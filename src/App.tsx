0a89b80af08f019bc067854832ce1c06f05c90bd
David Shaw <dshaw11@utk.edu>
2024-10-17 14:02:33 -0400

    removed my silly extra padding

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,8 @@ function App() {
       <NavBar />
       <main className='grid grid-rows-3'>
         <div className='bg-utk-blue--accent'>
-          <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white h-48'>Databases from UTK Libraries</h1>
+          <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white h-36'>Databases from UTK Libraries</h1>
         </div>
-
       </main>
       <Footer />
     </>

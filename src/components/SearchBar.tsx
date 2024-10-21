625bcbc5d7d3cf9ea93c8e57cc8081be9c83df2f
David Shaw <dshaw11@utk.edu>
2024-10-21 08:42:34 -0400

    progress on setting up my search bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,13 +4,14 @@ import { Form } from "react-router-dom";
 export default function SearchBar(props: any) {
     let { placeholder } = props;
     return (
-        <Form method="post" id="search-form">
+        <Form method="post" id="search-form" className="w-full mx-auto">
             <input
                 type="text"
                 placeholder={placeholder}
                 name="search"
+                className="w-3/4"
             />
-            <button type="submit">Search!</button>
+            <button type="submit" className="w-1/4 bg-utk-smokey">Search!</button>
         </Form>
     )
 }

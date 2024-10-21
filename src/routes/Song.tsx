db202de94cf778f5848fac2af702c89a5bdb4475
David Shaw <dshaw11@utk.edu>
2024-10-21 08:51:47 -0400

    styling for song and a bit of rearranging for the search component

--- src/routes/Song.tsx
+++ src/routes/Song.tsx
@@ -4,13 +4,9 @@ const Song = () => {
     return (
         <>
             <main className='lg:h-fit'>
-                <div className='h-32 bg-utk-blue--accent'>
-                    <div className="grid grid-rows-2 justify-center">
-                        <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white h-2/3 py-2'>Search the Song Database</h1>
-                        <div className="h-1/3 ">
-                            <SearchBar />
-                        </div>
-                    </div>
+                <div className='h-32 bg-utk-blue--accent grid grid-rows-2 justify-center my-auto py-auto'>
+                    <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2'>Search the Song Database</h1>
+                    <SearchBar />
 
                 </div>
                 <div>

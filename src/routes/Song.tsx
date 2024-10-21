5d19c8273b1f0037314af570f54e428f7b50a2c6
David Shaw <dshaw11@utk.edu>
2024-10-21 08:42:34 -0400

    progress on setting up my search bar component

--- src/routes/Song.tsx
+++ src/routes/Song.tsx
@@ -5,10 +5,13 @@ const Song = () => {
         <>
             <main className='lg:h-fit'>
                 <div className='h-32 bg-utk-blue--accent'>
-                    <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white h-36'>Search the Song Database</h1>
-                    <div className="">
-                        <SearchBar />
+                    <div className="grid grid-rows-2 justify-center">
+                        <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white h-2/3 py-2'>Search the Song Database</h1>
+                        <div className="h-1/3 ">
+                            <SearchBar />
+                        </div>
                     </div>
+
                 </div>
                 <div>
                     results go here

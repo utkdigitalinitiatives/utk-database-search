60f8cc94cbece73311cecb051816d968db8bbe2b
David Shaw <dshaw11@utk.edu>
2024-10-21 11:53:47 -0400

    finally fixed the stupid screen spacing so the footer is at the bottom

--- src/routes/Song.tsx
+++ src/routes/Song.tsx
@@ -5,7 +5,7 @@ const Song = () => {
         <>
             <main className='lg:h-fit'>
                 <div className='h-36 bg-utk-blue--accent grid grid-rows-2 justify-center my-auto py-2'>
-                    <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2'>Search the Song Database</h1>
+                    <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2'>Song Database</h1>
                     <div className="py-2 ">
                         <SearchBar
                             placeholder="Search the song database..."

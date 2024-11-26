2317a63388bdc279abb575eaf45468c55257e582
David Shaw <dshaw11@utk.edu>
2024-11-26 15:23:54 -0500

    working on styling and laying out the song result page

--- src/components/SongResults.tsx
+++ src/components/SongResults.tsx
@@ -4,19 +4,19 @@ export default function SongResults(props: any) {
     const resultList: any = props.resultList;
     console.log(resultList)
     return (
-        <>
+        <div className="mx-4 py-3 px-4 my-3">
             {resultList.map(result =>
                 // Build out a result component
-                <div key={result.new_id}>
-                    <div>
-                        Composers:{result.composers}
+                <div key={result.new_id} className="border-t border-utk-orange my-3 odd:bg-utk-light-gray even:bg-utk-white">
+                    <div className="flex justify-center text-utk-link font-medium text-xl">
+                        {result.title}
                     </div>
                     <div>
-                        call number:{result.call_number}
+                        call number:&nbsp;{result.call_number}
                     </div>
                 </div>
             )}
-        </>
+        </div>
     )
 
 }

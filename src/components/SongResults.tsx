09d86f456c12f9930fe8379e46fbd328bf1b183f
David Shaw <dshaw11@utk.edu>
2024-12-02 10:28:29 -0500

    more progress on setting up the search results for song/analysis db

--- src/components/SongResults.tsx
+++ src/components/SongResults.tsx
@@ -6,25 +6,25 @@ export default function SongResults(props: any) {
         <div className="mx-4 py-3 px-4 my-3">
             {props.resultList.map(result =>
                 // Build out a result component
-                <div key={result.new_id} className="border-t border-utk-orange my-3 odd:bg-utk-light-gray even:bg-utk-white shadow-md">
+                <div key={result.new_id} className="border-t border-utk-orange my-3 odd:bg-utk-light-gray even:bg-utk-white shadow-md py-3">
                     <div className="flex justify-center text-utk-link font-medium text-xl">
                         {result.title}
                     </div>
-                    <div className="flex flex-row flex-wrap justify-evenly">
-                        <div className="flex flex-col">
-                            Authors:
+                    <div className="flex flex-row flex-wrap justify-evenly text-utk-smokey py-2">
+                        <div className="flex flex-row flex-wrap">
+                            <span className="font-semibold px-2">Authors:</span>
                             {result.authors.map(author =>
-                                <div>{author}</div>
+                                <div className="text-wrap">{author}</div>
                             )}
                         </div>
-                        <div className="flex flex-col">
-                            <div>
-                                Song Type: {result.song_types}
+                        <div className="flex flex-col px-2">
+                            <div className="flex flex-wrap">
+                                <span className="font-semibold px-2">Song Type:</span> {result.song_types}
                             </div>
-                            <div className="flex flex-row">
-                                Languages:
+                            <div className="flex flex-row flex-wrap">
+                                <span className="font-semibold px-2">Languages:</span>
                             {result.languages.map(language =>
-                                <div>{language}</div>
+                                <div className="pe-1">{language}</div>
                             )}
                             </div>
                         </div>

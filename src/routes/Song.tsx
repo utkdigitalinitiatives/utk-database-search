4eb1cf78ff219ac190319e2b15b295d08d4b27dd
David Shaw <dshaw11@utk.edu>
2024-12-03 11:46:22 -0500

    more adjustments to the song index title and styling

--- src/routes/Song.tsx
+++ src/routes/Song.tsx
@@ -25,7 +25,7 @@ const Song = () => {
                 </div>
                 <div className="bg-[url('/src/assets/images/UT-bridge-campus.png')] bg-cover bg-center bg-slate-600 bg-blend-soft-light shadow-md">
                     <div className='h-36 grid grid-rows-2 justify-center my-auto py-2'>
-                        <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2 font-medium'>Song Database</h1>
+                        <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2 font-medium'>UT Song Index</h1>
                         <div className="py-2 ">
                             <SearchBar
                                 placeholder={placeholder}

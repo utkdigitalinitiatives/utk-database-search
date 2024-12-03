60c79cc503f8b6aefd39da49ae85405502da8cb3
David Shaw <dshaw11@utk.edu>
2024-12-03 14:16:42 -0500

    setup initial routing to individual song page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css'
 import Song from './routes/Song';
 import Index from './routes/Index';
+import SongPage from './routes/SongPage';
 
 
 
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />}>
           <Route index element={<Index />} />
           <Route path="/song" element={<Song />} />
+          <Route path="song/:songId" element={<SongPage />} />
         </Route>
       </Routes>
     </BrowserRouter>

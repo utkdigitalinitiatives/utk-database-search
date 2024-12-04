79b4ead8e1a5765f8fef5d4998ff86f623502f54
David Shaw <dshaw11@utk.edu>
2024-12-04 11:31:44 -0500

    working on the advanced search for the song database

---
+++ src/components/SongAdvancedSearch.tsx
@@ -0,0 +1,147 @@
+import { searchSolr } from "../utils/utils";
+import { useState } from "react";
+
+export default function SongAdvanced(props: any) {
+
+    const [query, setQuery] = useState('');
+    const [songTitle, setSongTitle] = useState('');
+    const [composer, setComposer] = useState('');
+    const [largeWork, setLargeWork] = useState('');
+    const [author, setAuthor] = useState('');
+    const [accompVal, setAccompVal] = useState('select');
+    const [songType, setSongType] = useState('select');
+    const [firstLine, setFirstLine] = useState('');
+    const [anthology, setAnthology] = useState('');
+    const [callNumber, setCallNumber] = useState('');
+    const [language, setLanguage] = useState('select')
+
+    const handleChange = (event: any) => {
+        setQuery((event.target.value));
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const params = new URLSearchParams({
+            q: `full_text:*${query}*`,
+            indent: "true",
+            wt: 'json',
+        })
+
+        const data = await searchSolr(`${props.endpoint}${params}`)
+        props.onSearch(data.response);
+
+    }
+
+    return (
+        <form method="post" id="search-form" className="w-full mx-auto" onSubmit={handleSubmit}>
+            <div className="flex flex-row">
+                <input
+                    type="text"
+                    placeholder="Song Title"
+                    name="songTitle"
+                    className="form-control shadow-inner border-2 focus:border-utk-orange focus:outline-none mt-2 p-1 rounded-md md:w-96 "
+                    onChange={e => setSongTitle(e.target.value)}
+                />
+            </div>
+
+            <div className="flex flex-row">
+                <input
+                    type="text"
+                    placeholder="Composer"
+                    name="composer"
+                    className="form-control shadow-inner border-2 focus:border-utk-orange focus:outline-none mt-2 p-1 rounded-md md:w-96 "
+                    onChange={e => setComposer(e.target.value)}
+                />
+            </div>
+
+            <div className="flex flex-row">
+                <input
+                    type="text"
+                    placeholder="Large Work"
+                    name="search"
+                    className="form-control shadow-inner border-2 focus:border-utk-orange focus:outline-none mt-2 p-1 rounded-md md:w-96 "
+                    onChange={e => setLargeWork(e.target.value)}
+                />
+            </div>
+
+            <div className="flex flex-row">
+                <input
+                    type="text"
+                    placeholder="Author"
+                    name="author"
+                    className="form-control shadow-inner border-2 focus:border-utk-orange focus:outline-none mt-2 p-1 rounded-md md:w-96 "
+                    onChange={e => setAuthor(e.target.value)}
+                />
+            </div>
+
+            <div className="flex flex-row">
+                <input
+                    type="text"
+                    placeholder="Anthology Title"
+                    name="anthology"
+                    className="form-control shadow-inner border-2 focus:border-utk-orange focus:outline-none mt-2 p-1 rounded-md md:w-96 "
+                    onChange={e => setAnthology(e.target.value)}
+                />
+            </div>
+
+            <div className="flex flex-row">
+                <input
+                    type="text"
+                    placeholder="Call Number"
+                    name="callNumber"
+                    className="form-control shadow-inner border-2 focus:border-utk-orange focus:outline-none mt-2 p-1 rounded-md md:w-96 "
+                    onChange={e => setCallNumber(e.target.value)}
+                />
+            </div>
+
+
+            <div className="flex flex-row mt-2">
+                <label className="text-utk-white mx-2">Select Song Type
+                    <select id="songType" name="songType" className="mx-2 text-utk-smokey" onChange={e => setSongType(e.target.value)}>
+                        <option value="select">Click Here</option>
+                        <option value="aria">Aria</option>
+                        <option value="art song">Art Song</option>
+                        <option value="carol">Carol</option>
+                        <option value="children's">Children's</option>
+                        <option value="folk">Folk</option>
+                        <option value="national anthem">National Anthem</option>
+                        <option value="patriotic">Patriotic</option>
+                        <option value="popular">Popular</option>
+                        <option value="sacred">Sacred</option>
+                        <option value="spiritual">Spiritual</option>
+                    </select>
+                </label>
+            </div>
+
+            <div className="flex flex-row mt-2">
+                <label className="text-utk-white mx-2">Accompaniment
+                    <select id="accompVal" name="accompVal" className="mx-2 text-utk-smokey" onChange={e => setAccompVal(e.target.value)}>
+                        <option value="select">Any Accompaniment</option>
+                        <option value="instrumental">Instrumental</option>
+                        <option value="keyboard">Keyboard</option>
+                        <option value="orchestra">Orchestra</option>
+                        <option value="unaccompanied">Unaccompanied</option>
+                    </select>
+                </label>
+            </div>
+
+            <div className="flex flex-row mt-2">
+                <label className="text-utk-white mx-2">Choose a language
+                    <select id="language" name="language" className="mx-2 text-utk-smokey" onChange={e => setLanguage(e.target.value)}>
+                        <option value="select">Any Language</option>
+                        <option value="english">English</option>
+                        <option value="french">French</option>
+                        <option value="german">German</option>
+                        <option value="italian">Italian</option>
+                        <option value="portuguese">Portuguese</option>
+                        <option value="spanish">Spanish</option>
+                    </select>
+                </label>
+            </div>
+            <div className="flex flex-row justify-end">
+                <button type="submit" className=" bg-[#dbdcde] border-2 mt-2 rounded-md text-utk-smokey hover:bg-utk-orange hover:text-utk-white hover:border-utk-orange text-center p-1 w-24">Search</button>
+            </div>
+        </form>
+
+    )
+}
--- src/routes/Song.tsx
+++ src/routes/Song.tsx
@@ -1,4 +1,5 @@
 import SingleSearchBar from "../components/SingleSearchBar";
+import SongAdvancedSearch from '../components/SongAdvancedSearch';
 import ResultHeader from "../components/ResultHeader";
 import { useState } from "react";
 import Breadcrumbs from "../components/Breadcrumbs";
@@ -36,7 +37,7 @@ const Song = () => {
                     <Breadcrumbs />
                 </div>
                 <div className="bg-[url('/src/assets/images/UT-bridge-campus.png')] bg-cover bg-center bg-slate-600 bg-blend-soft-light shadow-md">
-                    <div className='h-full grid grid-rows-2 justify-center my-auto py-3'> <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2 font-medium'>UT Song Index</h1>
+                    <div className='h-full grid justify-center my-auto py-3'> <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2 font-medium'>UT Song Index</h1>
                         {singleSearchVisible &&
                             <div className="py-2">
                                 <div className="flex flex-row-reverse">
@@ -56,8 +57,7 @@ const Song = () => {
                                 <div className="flex flex-row-reverse">
                                     <button className=" text-utk-white text-sm my-1" onClick={setAdvancedInvisible} >General</button>
                                 </div>
-                                <SingleSearchBar
-                                    placeholder={placeholder}
+                                <SongAdvancedSearch
                                     endpoint={endpoint}
                                     onSearch={handleSearchResults}
                                 />

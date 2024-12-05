97bac62a6129182b1c433e810023a2230ea951d5
David Shaw <dshaw11@utk.edu>
2024-12-05 09:39:56 -0500

    added instructions and information about song index to the song page

--- src/routes/Song.tsx
+++ src/routes/Song.tsx
@@ -37,7 +37,7 @@ const Song = () => {
                     <Breadcrumbs />
                 </div>
                 <div className="bg-[url('/src/assets/images/UT-bridge-campus.png')] bg-cover bg-center bg-slate-600 bg-blend-soft-light shadow-md">
-                    <div className='h-full grid justify-center my-auto py-3'> 
+                    <div className='h-full grid justify-center my-auto py-3'>
                         <h1 className='text-center flex justify-center items-center text-2xl md:text-4xl text-utk-white py-2 font-medium'>UT Song Index</h1>
                         {singleSearchVisible &&
                             <div className="bg-[rgba(75,75,75,0.90)] rounded-md">
@@ -65,9 +65,6 @@ const Song = () => {
                     </div>
                 </div>
                 <div className="container mx-auto max-w-screen-lg">
-                    <div>
-                    This database provides access to about 50,000 songs in more than 1,500 published song anthologies owned by the George F. DeVine Music Library at the University of Tennessee, located in Knoxville. Use this citation index to determine which anthologies contain the song(s) you need. You will not find the music or the words here, just the call number and book title. If you are not in Knoxville, ask your librarian about interlibrary loan options to obtain the songs you need be.
-                    </div>
                     {
                         totalFound > 0 ?
                             <ResultHeader totalRecords={totalFound} />
@@ -78,9 +75,28 @@ const Song = () => {
                         results.length > 0 ?
                             <SongResults resultList={results} />
                             :
-                            <div className="flex justify-center items-center min-h-96">
-                                Enter a term to search for above to begin
-                            </div>
+                            <>
+                                <div className="px-2 text-sm text-utk-smokey mt-2">
+                                    This database provides access to about 50,000 songs in more than 1,500 published song anthologies owned by the George F. DeVine Music Library at the University of Tennessee, located in Knoxville. Use this citation index to determine which anthologies contain the song(s) you need. You will not find the music or the words here, just the call number and book title. If you are not in Knoxville, ask your librarian about interlibrary loan options to obtain the songs you need be.
+                                </div>
+                                <div className="text-sm text-utk-smokey px-2">
+                                    <p className="font-semibold my-2">INSTRUCTIONS</p>
+                                    <ol className="list-disc px-4 mt-2">
+                                        <li>
+                                            Start your search as simply as possible. For example, enter the last name of the composer and one key word from the song title. However, more than one keyword and any number of fields can be used.
+                                        </li>
+                                        <li className="pt-1">
+                                            DO NOT use initial articles such as: the/of/a/an  (use 'Sound of Music' rather than 'The Sound of Music').
+                                        </li>
+                                        <li className="pt-1">
+                                            DO NOT use dashes or punctuation, besides apostrophes.
+                                        </li>
+                                        <li className="pt-1">
+                                            Truncation is automatic ('Tchai' will yield 'Tchaikovsky').
+                                        </li>
+                                    </ol>
+                                </div>
+                            </>
                     }
                 </div>
             </main>

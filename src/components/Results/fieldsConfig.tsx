3820139c9c8eeb7e3037f0791d819c129767d2d1
David Shaw <dshaw11@utk.edu>
2024-12-13 12:45:09 -0500

    setup so the result page now can accept arrays

---
+++ src/components/Results/fieldsConfig.tsx
@@ -0,0 +1,64 @@
+interface Field {
+    label: string;
+    key: string;
+    type: 'string' | 'array';  // You can add more types as necessary
+    render?: (data: any) => JSX.Element;  // This should always return a JSX.Element
+}
+
+interface ResultConfig {
+    titleField: string;
+    idField: string;
+    fields: Field[];
+}
+
+export const fieldsConfig: { [key: string]: ResultConfig } = {
+    sermon: {
+        titleField: "short_title", // Name of the title field in this result
+        idField: "id",            // Name of the ID field in this result
+        fields: [
+            {
+                label: "Denomination",
+                key: "denomination",
+                type: "string",
+            },
+            {
+                label: "Author",
+                key: "author",
+                type: "string",
+            },
+            {
+                label: "State",
+                key: "state",
+                type: "string",
+            },
+            {
+                label: "Dates",
+                key: "dates",
+                type: "string",
+            },
+            
+            // Add more fields as necessary
+        ]
+    },
+    song: {
+        titleField: 'title',
+        idField: 'id',
+        fields: [
+            {
+                label: 'Composers',
+                key: 'composers',
+                type: 'array',
+                render: (data: string[]): JSX.Element => {
+                    // Ensure it returns JSX elements, not boolean
+                    return (
+                        <div className="flex flex-col">
+                            {data.map((author, index) => (
+                                <div key={index} className="text-wrap">{author}</div>
+                            ))}
+                        </div>
+                    );
+                },
+            },
+        ]
+    }
+};

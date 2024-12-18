a15daa5cbf5661e5042df04dfd9e5df014e94dff
David Shaw <dshaw11@utk.edu>
2024-12-18 13:27:46 -0500

    added and fixed some styling on the individual page

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -29,11 +29,11 @@ export default function Pager(props: any) {
         <>
             <div className="flex flex-row justify-center my-2">
                 {props.searchStart > 0 ?
-                    <button className="mx-2 p-2 border rounded-md border-utk-smokey shadow-lg" onClick={handlePrevious}>Previous</button>
+                    <button className="mx-2 p-2 border rounded-md border-utk-orange shadow-lg ease-in-out duration-300 hover:scale-105" onClick={handlePrevious}>Previous</button>
                     :
                     <button disabled className="mx-2 p-2 border bg-neutral-300 text-neutral-50 rounded-md cursor-not-allowed" >Previous</button>
                 }
-                <button className="mx-2 p-2 border rounded-md border-utk-smokey shadow-lg" onClick={handleNext}>Next</button>
+                <button className="mx-2 p-2 border rounded-md border-utk-orange shadow-lg ease-in-out duration-300 hover:scale-105" onClick={handleNext}>Next</button>
             </div>
         </>
     )
--- src/components/SearchResultsList.tsx
+++ src/components/SearchResultsList.tsx
@@ -11,7 +11,7 @@ export default function SearchResultsList({ resultList, resultType }: SearchResu
     return (
         <div className="mx-4 py-3 px-4 my-3 utk-link">
             {resultList?.map(result => (
-                <div key={result?.[config.idField]} className="border-t border-utk-orange my-3 odd:bg-utk-light-gray even:bg-utk-white shadow-md py-3 px-2">
+                <div key={result?.[config.idField]} className="border-t rounded-sm border-utk-orange my-3 odd:bg-utk-light-gray even:bg-utk-white shadow-md py-3 px-2">
                     <div className="flex justify-center text-utk-link font-medium text-xl">
                         {result?.[config.titleField]}
                     </div>
@@ -46,7 +46,7 @@ export default function SearchResultsList({ resultList, resultType }: SearchResu
                         })}
                     </div>
                     <div className="flex flex-row flex-wrap justify-end text-utk-smokey max-w-108 mx-auto">
-                        <Link to={`${result?.[config.idField]}`} className="flex flex-row px-2">View All Details
+                        <Link to={`${result?.[config.idField]}`} className="flex flex-row px-2 ease-in-out duration-300 hover:scale-x-105">View All Details
                             <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 2.001c5.524 0 10 4.477 10 10s-4.476 10-10 10c-5.522 0-10-4.477-10-10s4.478-10 10-10Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17Zm-.352 4.053.072-.084a.75.75 0 0 1 .977-.073l.084.073 4 4a.75.75 0 0 1 .073.977l-.072.085-4.002 4a.75.75 0 0 1-1.133-.977l.073-.084 2.722-2.721H7.75a.75.75 0 0 1-.743-.648L7 12a.75.75 0 0 1 .648-.743l.102-.007h6.69l-2.72-2.72a.75.75 0 0 1-.072-.976l.072-.084-.072.084Z" fill="#ff8200" />
                             </svg>

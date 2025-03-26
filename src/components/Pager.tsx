dd90d231a7aeeff90c9373b83eea13fec35db673
David Shaw <dshaw11@utk.edu>
2025-03-26 13:35:24 -0400

    formatting fix on pager component

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -44,7 +44,7 @@ export default function Pager<T>(props: PagerProps<T>) {
                     <button className="mx-2 p-2 border rounded-md border-utk-orange shadow-lg ease-in-out duration-300 hover:scale-105" onClick={handleNext}>Next</button>
                     :
                     <button disabled className="mx-2 p-2 border bg-neutral-300 text-neutral-50 rounded-md cursor-not-allowed">Next</button>
-            }
+                }
             </div>
         </>
     )
